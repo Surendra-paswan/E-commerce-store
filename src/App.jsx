76862db4a7f741d02ed4b0b1e8d08ec4300fdb6b
Surendra-paswan <surendramoglaha@gmail.com>
2025-05-07 14:28:02 +0545

    Ract training

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import Body from './components/Body'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Body />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
